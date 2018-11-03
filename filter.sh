90b63c1986f482645e96580c94be774fb9b2d12c
Robert Kulyassa <qji@elte.hu>
2018-11-03 18:33:46 +0100

    filter.sh: change output name and path, create all-results.dat

--- filter.sh
+++ filter.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(dirname $0)
+
+HOSTNAME_HASH=$(hostname | md5sum | cut -d' ' -f1)
+
 awk 'BEGIN {p=0; s=0}
      $3!=p || $4!=s {print; p=$3; s=$4}' \
-    ~/index-tamogatas.dat > ~/index-tamogatas.dat.filtered
+    ~/index-tamogatas.dat > $SCRIPT_PATH/results/${HOSTNAME_HASH}-index-tamogatas.dat.filtered
+
+cat $SCRIPT_PATH/results/*-index-tamogatas.dat.filtered | sort -n -k2 | uniq > results/all-results.dat
 
